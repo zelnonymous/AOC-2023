20840f27f3483c808bc226c564969750f8d7ced3
zelnonymous <75436152+zelnonymous@users.noreply.github.com>
2023-12-02 14:25:44 -0500

    Fixed a parser bug and completed part 1

--- Day 2/day2.go
+++ Day 2/day2.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -14,7 +15,13 @@ type pull struct {
 }
 
 func main() {
-    games := make(map[string][]pull)
+    p1limits := map[string]int{
+        "red": 12,
+        "green": 13,
+        "blue": 14,
+    }
+    p1possible := 0
+    games := make(map[int][]pull)
     file, err := os.Open("input.txt")
     if err != nil {
         log.Fatal(err)
@@ -25,19 +32,59 @@ func main() {
         gameid, pulls := ParseGame(line) 
         games[gameid] = pulls
     }
+    ordered_gameids := make([]int, 0)
+    for gameid := range games {
+        ordered_gameids = append(ordered_gameids, gameid)
+    }
+    sort.Ints(ordered_gameids)
+    for _, gameid := range ordered_gameids {
+        pulls := games[gameid]
+        possible := true
+        for _, pullinfo := range pulls {
+            for color, qty := range pullinfo.cubes {
+                if qty > p1limits[color] {
+                    fmt.Printf(
+                        "In game %v, pulled %v %v but limit was %v\n",
+                        gameid,
+                        qty,
+                        color,
+                        p1limits[color],
+                    )
+                    possible = false
+                    break
+                }
+            }
+            if possible == false {
+                break
+            }
+        }
+        if possible {
+            fmt.Printf(
+                "Game %v: Possible, Running Total: %v\n", 
+                gameid, 
+                p1possible)
+            p1possible += gameid
+        } 
+    }
+    fmt.Printf("Part 1: Sum of possible game IDs: %v\n", p1possible)
 
 }
-func ParseGame(line string) (string, []pull) {
+func ParseGame(line string) (int, []pull) {
     gameinfo := strings.Split(line, ":")
     if len(gameinfo) < 2 {
         log.Fatal("Game with no data detected!")
+    }
+    idstr := strings.TrimLeft(gameinfo[0], "Game ")
+    id, err := strconv.Atoi(idstr)
+    if err != nil {
+        log.Fatal("Game with non-numeric ID detected!")
     }
     var result []pull
     pullstrings := strings.Split(gameinfo[1], ";")
     for _, pullstring := range pullstrings {
         result = append(result, ParsePull(pullstring))
     }
-    return gameinfo[0], result
+    return id, result
 }
 func ParsePull(pullstring string) pull {
     cubes := make(map[string]int)
@@ -55,12 +102,13 @@ func ParsePull(pullstring string) pull {
     }
 }
 func ParsePullCubes(info string) (string, int, error) {
+    info = strings.TrimSpace(info)
     details := strings.Split(info, " ")
     if len(details) < 2 {
         return "", 0, errors.New("Found cube pull without color or count")
     }
-    color := strings.TrimSpace(details[0])
-    qty, err := strconv.Atoi(details[1])
+    color := strings.TrimSpace(details[1])
+    qty, err := strconv.Atoi(details[0])
     if err != nil {
         return color, 0, errors.New("Warning: Cube quantity is not a number")
     }
