5afa247eda075950c7f674a6fd59e0ae700330a7
Aaron Brown <zelnot@gmail.com>
2023-12-13 16:45:22 -0500

    WIP Day 13 Part 2 Brute Force Solution

--- Day 13/day13.go
+++ Day 13/day13.go
@@ -8,6 +8,7 @@ import (
 )
 type Pattern struct {
     data [][]rune
+    score int
 }
 var patterns []Pattern
 func main() {
@@ -31,68 +32,103 @@ func main() {
         patterns = append(patterns, current)
     }
     p1Total := 0
-    SEARCHPATTERNS:
+    p2Total := 0
+    PATTERNLOOP:
     for i,pattern := range patterns {
         fmt.Printf("Checking pattern %v\n", i+1)
         for row := range pattern.data {
             fmt.Println(string(pattern.data[row]))
         }
         fmt.Println()
-        SEARCHCOLUMNS:
-        for col := 1; col < len(pattern.data[0]); col++ {
-            for row := range pattern.data {
-                left := string(pattern.data[row][0:col])
-                right := string(pattern.data[row][col:])
-                left = reverse(left)
-                if len(left) < len(right) {
-                    right = right[0:len(left)]
-                } else if len(right) < len(left) {
-                    left = left[0:len(right)]
-                }
-                if left != right {
-                    continue SEARCHCOLUMNS
-                }
-            }
-            p1Total += col
-            fmt.Printf("Found mirror at column %v, adding %v to total\n\n", 
-                col, col)
-            continue SEARCHPATTERNS
+        FindMirrors(&pattern, -1, false)
+        p1Total += pattern.score
+        originalScore := pattern.score
+        fmt.Println("Original:")
+        for _,prow := range pattern.data {
+            fmt.Println(string(prow))
         }
+        for row := range pattern.data {
+            for col := range pattern.data[row] {
+                origChar := pattern.data[row][col]
+                if origChar == '.' {
+                    pattern.data[row][col] = '#'
+                } else {
+                    pattern.data[row][col] = '.'
+                }
+                FindMirrors(&pattern, originalScore, false)
+                if pattern.score != originalScore {
+                    fmt.Println("Successful Mutation:")
+                    for _,prow := range pattern.data {
+                        fmt.Println(string(prow))
+                    }
+                    fmt.Printf("Original score: %v, Mutation score: %v\n", originalScore,
+                        pattern.score)
+                    p2Total += pattern.score
+                    continue PATTERNLOOP
+                }
+                pattern.data[row][col] = origChar
+            }
+        }
+
+    }
+    fmt.Printf("Part 1: Total: %v\n", p1Total)
+    fmt.Printf("Part 2: Total: %v\n", p2Total)
+}
+func FindMirrors(pattern *Pattern, original int, debug bool) {
+    SEARCHCOLUMNS:
+    for col := 1; col < len(pattern.data[0]); col++ {
+        for row := range pattern.data {
+            left := string(pattern.data[row][0:col])
+            right := string(pattern.data[row][col:])
+            left = reverse(left)
+            if len(left) < len(right) {
+                right = right[0:len(left)]
+            } else if len(right) < len(left) {
+                left = left[0:len(right)]
+            }
+            if left != right || col == original {
+                continue SEARCHCOLUMNS
+            }
+        }
+        pattern.score = col
+        return 
+    }
+    if debug {
         fmt.Println("No mirror columns found.  Checking rows.")
-        SEARCHROWS:
-        for row := 1; row < len(pattern.data); row ++ {
-            top := pattern.data[0:row]
-            bottom := pattern.data[row:]
-            if len(top) < len(bottom) {
-                bottom = bottom[0:len(top)]
-            }
-            if len(bottom) < len(top) {
-                top = top[len(top) - len(bottom):]
-            }
-            top = reverseRows(top)
+    }
+    SEARCHROWS:
+    for row := 1; row < len(pattern.data); row ++ {
+        top := pattern.data[0:row]
+        bottom := pattern.data[row:]
+        if len(top) < len(bottom) {
+            bottom = bottom[0:len(top)]
+        }
+        if len(bottom) < len(top) {
+            top = top[len(top) - len(bottom):]
+        }
+        top = reverseRows(top)
+        if debug {
             fmt.Printf("ROW: %v\n", row)
             fmt.Println("Top:           Bottom:")
             for tr := range top {
                 fmt.Printf("%v",string(top[tr]))
                 fmt.Printf("   %v\n",string(bottom[tr]))
             }
-            for row := range top {
-                for col := range top[row] {
-                    if top[row][col] != bottom[row][col] {
-                        continue SEARCHROWS
-                    }
+        }
+        for row := range top {
+            for col := range top[row] {
+                if top[row][col] != bottom[row][col] || 
+                    row * 100 == original {
+                    continue SEARCHROWS
                 }
             }
-            rowval := row * 100
-            fmt.Printf("Found mirror at row %v, adding %v to total\n\n", 
-                row, rowval)
-            p1Total += rowval 
-            continue SEARCHPATTERNS
         }
-        log.Panic("NO MIRROR FOUND!")
+        pattern.score = row * 100
+        return
     }
-    fmt.Printf("Part 1: Total: %v\n", p1Total)
-
+    if debug {
+        fmt.Println("WARNING: MIRROR NOT FOUND!")
+    }
 }
 func reverseRows(input [][]rune) [][]rune {
     output := make([][]rune, len(input))
